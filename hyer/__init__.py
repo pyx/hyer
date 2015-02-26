35c8bd4d6946eaed0def2538128bd2e2e418fa46
Philip Xu <pyx@xrefactor.com>
2015-02-26 18:11:16 -0500

    Created package hyer and hyer.macros

---
+++ hyer/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014-2015, Philip Xu <pyx@xrefactor.com>
+# License: BSD New, see LICENSE for details.
+"""Hyer - Hy enhanced routing"""
+
+__version__ = (0, 1)
+__release__ = '.dev0'
+
+VERSION = '%d.%d' % __version__ + __release__
+
+__all__ = [
+]
