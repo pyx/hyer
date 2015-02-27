276cc99bf9e7f81a2a2cca2e36ccef2f759b9f1f
Philip Xu <pyx@xrefactor.com>
2015-02-26 23:13:11 -0500

    Added examples

---
+++ examples/hello/app.hy
@@ -0,0 +1,22 @@
+#!/usr/bin/env hy
+;; -*- coding: utf-8 -*-
+;; Copyright (c) 2014-2015, Philip Xu <pyx@xrefactor.com>
+;; License: BSD New, see LICENSE for details.
+"Hello world example"
+
+(import [flask [Flask]])
+
+(require hyer.dsl)
+
+(defn create-app []
+  "application factory"
+  (defapplication app
+    (GET "/" index
+      "Hello world")
+    (GET "/<username>/" greating
+      (.format "Hello, {}!" username))
+    (GET "/<int:a>+<int:b>/" addition
+      (.format "{} + {} = {}" a b (+ a b)))))
+
+(defmain [&rest args]
+  (-> create-app apply .run))
---
+++ examples/hello/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env hy
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014-2015, Philip Xu <pyx@xrefactor.com>
+# License: BSD New, see LICENSE for details.
+"Hello world example"
+
+from flask import Flask
+
+def create_app():
+    """application factory"""
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return 'Hello world'
+
+    @app.route('/<username>/')
+    def greating(username):
+        return 'Hello, {}!'.format(username)
+
+    @app.route('/<int:a>+<int:b>/')
+    def addition(a, b):
+        return '{} + {} = {}'.format(a, b, a + b)
+
+    return app
+
+
+if __name__ == '__main__':
+    create_app().run()
